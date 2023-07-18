878ba9994f866d82e495d9c5543f8e92292826c3
Hudson <hudsonbeandev@gmail.com>
2023-07-18 00:33:57 -0500

    Initialized server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+//Services
+const express = require("express");
+
+//Variables
+const APP = express();
+const PORT = process.env.PORT || 8080;
+
+//Function
+
+// Start server
+APP.listen(PORT, () => {
+  console.log(
+    `Server is active at \x1b[34;1;4mhttp://localhost:${PORT}/\x1b[0m`
+  );
+});
+
+// Routing
+APP.get("/api/dev", (req, res) => {
+  console.log(req.content);
+  res.status(200).send("Nothing to see here 😳").json("Hello from server!");
+});
